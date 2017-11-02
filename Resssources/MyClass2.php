01adc9d74a367c8356aa4b69d90e21cbde50d401
RPetrosjan <rubenann@yahoo.com>
2017-11-02 16:51:55 +0100

    Here ist de commit example

---
+++ Resssources/MyClass2.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rpetrosjan
+ * Date: 02/11/2017
+ * Time: 16:48
+ */
+class MyClass2
+{
+    public function myClass(){
+        $nom = 'Ruben';
+    }
+}
---
+++ Resssources/MyClassComit.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rpetrosjan
+ * Date: 02/11/2017
+ * Time: 16:48
+ */
+class MyClassComit
+{
+
+}
